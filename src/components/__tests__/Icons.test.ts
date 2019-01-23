de34c8aa5b67cd647894ef22e42c9326a75e5061
Marco Martins <marcodanielmartins@gmail.com>
2019-01-23 17:26:19 +0100

    :cop: Icons viewBox, rotation and test updates

--- src/components/__tests__/Icons.test.ts
+++ src/components/__tests__/Icons.test.ts
@@ -1,9 +1,14 @@
 import * as Icons from '../Icons'
 
 describe('Icons List', () => {
-	it('should check for icons base properties', () => {
-		expect(Icons.user).toHaveProperty('name')
-		expect(Icons.user).toHaveProperty('viewBox')
-		expect(Icons.user).toHaveProperty('draw')
+	it('should check all icons have base properties', () => {
+		const Icon = Icons as Object;
+		for (const key in Icon) {
+			if (Icon.hasOwnProperty(key)) {
+				expect(Icon[key]).toHaveProperty('name')
+				expect(Icon[key]).toHaveProperty('viewBox')
+				expect(Icon[key]).toHaveProperty('draw')
+			}
+		}
 	})
 })
