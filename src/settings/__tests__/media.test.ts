49e61f2b0e9fd3d7079df64bfb172081513fb7db
Marco Martins <marcodanielmartins@gmail.com>
2019-08-09 06:19:09 +0200

    :robot: Update media tests

--- src/settings/__tests__/media.test.ts
+++ src/settings/__tests__/media.test.ts
@@ -1,33 +1,34 @@
 import { media } from '../media'
+import { FlattenSimpleInterpolation } from 'styled-components'
+
+const formatCss = (css: FlattenSimpleInterpolation) => {
+	return css.join('').replace(/\s/g, '')
+}
 
 describe('Media Queries', () => {
 	it('should match phone media query', () => {
 		const phone = media.phone`
 			max-width: 100px;
 		`
-
-		const css = phone.join('').replace(/\s/g, '')
-
-		expect(css).toEqual('@media(min-width:320px){max-width:100px;}')
+		expect(formatCss(phone)).toEqual('@media(min-width:320px){max-width:100px;}')
 	})
 
 	it('should match tablet media query', () => {
 		const tablet = media.tablet`
 			max-width: 100px;
 		`
-
-		const css = tablet.join('').replace(/\s/g, '')
-
-		expect(css).toEqual('@media(min-width:768px){max-width:100px;}')
+		expect(formatCss(tablet)).toEqual('@media(min-width:768px){max-width:100px;}')
 	})
 
 	it('should match desktop media query', () => {
 		const desktop = media.desktop`
 			max-width: 100px;
 		`
+		expect(formatCss(desktop)).toEqual('@media(min-width:1224px){max-width:100px;}')
+	})
 
-		const css = desktop.join('').replace(/\s/g, '')
-
-		expect(css).toEqual('@media(min-width:1224px){max-width:100px;}')
+	it('should match css properties with interpolations', () => {
+		const tablet = media.tablet({}, `display: none;`)
+		expect(formatCss(tablet)).toEqual('@media(min-width:768px){display:none;}')
 	})
 })
